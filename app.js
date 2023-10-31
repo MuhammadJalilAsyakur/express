af7b4ae39b84f8f2afd7d67852bd0d74ce473e88
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-10-31 14:25:03 +0700

    m

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const rootRoutes = require('./routes');
 const bodyParser = require('body-parser');
 
 
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(rootRoutes);
